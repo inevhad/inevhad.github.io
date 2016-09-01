23e6f9b44404209f0ead4a3c7f1d889f8022a2aa
inevhad <ine.sena@gmail.com>
2016-09-01 23:17:24 +0700

    update everything

--- creative/js1/myjs.js
+++ creative/js1/myjs.js
@@ -1,7 +1,69 @@
-$('.desainkoe').magnificPopup({
+/*$('.desainkoe').magnificPopup({
   delegate: 'a', // child items selector, by clicking on it popup will open
   type: 'image'
   // other options
 });
 
 
+$(document).ready(function() {
+	$('.desainkoe').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+}); 
+*/
+$('.desainkoe').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    }); 
+
+/*$('.desainkoe').magnificPopup({
+    items: [
+      {
+        src: 'img/xlogo/1.jpg'
+      },
+      {
+        src: 'http://vimeo.com/123123',
+        type: 'iframe' // this overrides default type
+      },
+      {
+        src: $('<div>Dynamically created element</div>'), // Dynamically created element
+        type: 'inline'
+      },
+      {
+        src: '<div>HTML string</div>',
+        type: 'inline'
+      },
+      {
+        src: '#my-popup', // CSS selector of an element on page that should be used as a popup
+        type: 'inline'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});*/
